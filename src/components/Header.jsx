d0e94c0642b97a7e27d529e0429151a83368fcbf
Henaki <mihailboe@mail.ru>
2022-02-01 21:45:19 +0400

    Сверстана главная страница

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,54 @@
 import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo.png";
+import style from "./Components.module.css";
 
 export const Header = () => {
   return (
-    <>
-      Header
-      <Link to="/">Главная</Link>
-      <Link to="/QnA">Q&A</Link>
-      <Link to="/feedback">Обратная связь</Link>
-      <Link to="/login">Вход</Link>
-    </>
+    <header>
+      <img src={Logo} alt="logo" className={style.header__logo} />
+      <div className={style.hamburger_menu}>
+        <input id="menutoggle" className={style.menu__toggle} type="checkbox" />
+        <label class={style.menu__btn} for="menutoggle">
+          <span></span>
+        </label>
+
+        <ul className={style.menu__box}>
+          <li>
+            <Link className={style.header_list_link} to="/">
+              Главная
+            </Link>
+          </li>
+          <li>
+            <Link className={style.header_list_link} to="/QnA">
+              Вопросы-ответы
+            </Link>
+          </li>
+          <li>
+            <Link className={style.header_list_link} to="/feedback">
+              Обратная связь
+            </Link>
+          </li>
+          <li>
+            <Link className={style.header_list_link} to="/login">
+              Вход
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={style.header_list}>
+        <Link className={style.header_list_link} to="/">
+          Главная
+        </Link>
+        <Link className={style.header_list_link} to="/QnA">
+          Вопросы-ответы
+        </Link>
+        <Link className={style.header_list_link} to="/feedback">
+          Обратная связь
+        </Link>
+        <Link className={style.header_list_link} to="/login">
+          Вход
+        </Link>
+      </div>
+    </header>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,55 @@
+import style from "./Pages.module.css";
+import image_card_1 from "../assets/images/opportunity.png";
+import image_card_2 from "../assets/images/opportunity-2.png";
+import image_card_3 from "../assets/images/opportunity-3.png";
+import image_card_4 from "../assets/images/opportunity-4.png";
+import image_card_5 from "../assets/images/opportunity-5.png";
+import image_card_6 from "../assets/images/opportunity-6.png";
+import image_slide from "../assets/images/O0WDcGN8wII.jpg";
+
 export const Home = () => {
-  return <>
-    Home
-  </>
-}
+  return (
+    <section className={style.homepage}>
+      <h1>С нами ты можешь</h1>
+      <section className={style.homepage_section}>
+        <div className={style.section_card}>
+          <img src={image_card_1} alt="1imgcard" />
+          <p>
+            Безотрывно от учебы закрепить пройденные темы, проработать задания
+            ОГЭ и ЕГЭ
+          </p>
+        </div>
+        <div className={style.section_card}>
+          <img src={image_card_2} alt="1imgcard" />
+          <p>Самостоятельно определять время, место и условия обучения</p>
+        </div>
+        <div className={style.section_card}>
+          <img src={image_card_3} alt="1imgcard" />
+          <p>
+            Не беспокоиться о том, что отстанешь от своих одноклассников, ведь
+            темп обучения выбираешь ты сам
+          </p>
+        </div>
+        <div className={style.section_card}>
+          <img src={image_card_4} alt="1imgcard" />
+          <p>Найти новых друзей</p>
+        </div>
+        <div className={style.section_card}>
+          <img src={image_card_5} alt="1imgcard" />
+          <p>Получить подарки</p>
+        </div>
+        <div className={style.section_card}>
+          <img src={image_card_6} alt="1imgcard" />
+          <p>Стать экспертом</p>
+        </div>
+      </section>
+      <section className={style.homepage_news}>
+        <h2>Новости</h2>
+        <div className={style.homepage_news__slider}>
+          <img src={image_slide} alt="slide1" />
+          <input type="radio" />
+        </div>
+      </section>
+    </section>
+  );
+};
