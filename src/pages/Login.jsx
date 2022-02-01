ec575b55265a783d7f0f1bef45c448d2fbd04a7c
Henaki <mihailboe@mail.ru>
2022-02-01 19:10:18 +0400

    Test

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 export const Login = () => {
   return (
     <>
-      Login
-      <input type="text" name="" id="" />
       <Link to="/signup">Регистр</Link>
     </>
   );
