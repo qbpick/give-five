56988acc75f9527f9e32bcdc925cc5b0a484b3f1
Henaki <mihailboe@mail.ru>
2022-02-03 23:08:22 +0400

    add QnA and Feedback pages layots

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,5 +1,14 @@
+import style from "./Pages.module.css";
 export const Feedback = () => {
-  return <>
-    Feedback
-  </>
-}
+  return (
+    <section className={style.section_feedback}>
+      <p>
+        Наша группа в <a href="vk.com">Вконтакте</a>
+      </p>
+      <p>
+        Наш аккаунт в <a href="vk.com">Инстаграме</a>
+      </p>
+      <p>Наша почта: test@high-five.site</p>
+    </section>
+  );
+};
