ddf4bfc0efbba64b70dbd712d9952b5ce98fddd1
Henaki <mihailboe@mail.ru>
2022-03-09 21:44:56 +0400

    add result test and give me suck ur dick pls last day to TRAHAT'

--- src/pages/ResultTest.jsx
+++ src/pages/ResultTest.jsx
@@ -1,11 +1,17 @@
 import style from "./Pages.module.css";
 
 export const ResultTest = () => {
+    const data = {
+        max: localStorage.getItem("questions"),
+        right: localStorage.getItem("result")
+    }
+    let all_right = data.right / data.max * 100 
     return (
         <section className={style.section_result}>
             <h2>Поздравляем! вы ДЕБИЛ</h2>
             <p>Название теста: {"Грамматика"}</p>
-            <p>Ваш итоговый балл: {47} из 46</p>
+            <p>Ваш итоговый балл: {data.right} из {data.max}</p>
+            <p>Ваша оценка: {Math.round(all_right, -1)}%</p>
         </section>
     )
 }
